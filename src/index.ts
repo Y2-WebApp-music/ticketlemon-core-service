de463885b55944d7a20fe519acc7ca589b9e06a0
Ariez <kritamet.cho@gmail.com>
2026-03-27 14:18:45 +0700

    Setup project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const port = Number(process.env.PORT) || 8003;
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
